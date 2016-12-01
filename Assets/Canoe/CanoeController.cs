fa76cc5cdecf41ff9b9eb516f24c396ad0e7b43a
Sebastiran <Sebastiaan.vandijk@hotmail.com>
2016-12-01 11:50:46 +0100

    Canoe movement

--- Assets/Canoe/CanoeController.cs
+++ Assets/Canoe/CanoeController.cs
@@ -1,15 +1,27 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class CanoeController : MonoBehaviour {
+[RequireComponent(typeof(Rigidbody))]
+public class CanoeController : MonoBehaviour
+{
+    Rigidbody mRigidbody;
+    Vector3 moveDirection;
+    float peddleForce = 1000;
+    [SerializeField] float maxSpeed = 50f;
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+    void Start()
+    {
+        mRigidbody = GetComponent<Rigidbody>();
+        moveDirection = Vector3.zero;
+    }
+
+    void Update()
+    {
+        float gas = Input.GetAxis("Vertical");
+        float steer = Input.GetAxis("Horizontal");
+        mRigidbody.AddForce(transform.forward * gas * peddleForce * Time.deltaTime);
+        transform.Rotate(0, steer, 0);
+        if (mRigidbody.velocity.magnitude > maxSpeed)
+            mRigidbody.velocity = mRigidbody.velocity.normalized * maxSpeed;
+    }
 }
