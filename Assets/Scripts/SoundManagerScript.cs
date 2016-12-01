04e8295453c4b59829fd0317788659fec25e20ea
Daan Levendig <d.levendig@hva.nl>
2016-12-01 15:46:33 +0100

    - Tree prefab - Stone prefab - SoundManager - End of game UI

---
+++ Assets/Scripts/SoundManagerScript.cs
@@ -0,0 +1,8 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SoundManagerScript : MonoBehaviour 
+{
+	public AudioSource music, hit, shoot, splash, dive, bubble, thunder;
+
+}
---
+++ Assets/Scripts/UIscript.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class UIscript : MonoBehaviour 
+{
+	public GameObject pOneWin, pOneLose, pTwoWin, pTwoLose, bgOverlay;
+	public int playerIsWinner;
+	public bool endGame;
+
+	void Start()
+	{
+		playerIsWinner = 0;
+		endGame = false;
+	}
+	// Update is called once per frame
+	void Update ()
+	{
+		if (endGame)
+			bgOverlay.SetActive(true);
+		else
+			bgOverlay.SetActive(false);
+
+		switch (playerIsWinner)
+		{
+			case 0:
+				pOneWin.SetActive(false);
+				pOneLose.SetActive(false);
+				pTwoWin.SetActive(false);
+				pTwoLose.SetActive(false);
+				break;
+			case 1:
+				pOneWin.SetActive(true);
+				pTwoLose.SetActive(true);
+				break;
+			case 2:
+				pOneLose.SetActive(true);
+				pTwoWin.SetActive(true);
+				break;
+			default:
+				break;
+		}
+
+	}
+}
