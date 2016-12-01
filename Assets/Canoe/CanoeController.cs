5e7d2d684d69c8d451ef362504cdcd70533dcc59
Sebastiran <Sebastiaan.vandijk@hotmail.com>
2016-12-01 12:07:53 +0100

    Splitscreen two players

--- Assets/Canoe/CanoeController.cs
+++ Assets/Canoe/CanoeController.cs
@@ -4,9 +4,11 @@ using System.Collections;
 [RequireComponent(typeof(Rigidbody))]
 public class CanoeController : MonoBehaviour
 {
+    
     Rigidbody mRigidbody;
     Vector3 moveDirection;
     float peddleForce = 1000;
+    [SerializeField] string player;
     [SerializeField] float maxSpeed = 50f;
 
     void Start()
@@ -17,8 +19,8 @@ public class CanoeController : MonoBehaviour
 
     void Update()
     {
-        float gas = Input.GetAxis("Vertical");
-        float steer = Input.GetAxis("Horizontal");
+        float gas = Input.GetAxis("Vertical_" + player);
+        float steer = Input.GetAxis("Horizontal_" + player);
         mRigidbody.AddForce(transform.forward * gas * peddleForce * Time.deltaTime);
         transform.Rotate(0, steer, 0);
         if (mRigidbody.velocity.magnitude > maxSpeed)
